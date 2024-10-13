aa72c919c91311104abaeb2ca968c5b1a4ae1472
Oliver-Shariff <143436031+Oliver-Shariff@users.noreply.github.com>
2024-10-13 19:34:31 -0400

    echart implemented
    An echart using random data that updates every 100ms is visible on the dashboard now

--- ble_captures/static/ble_captures/js/javascript.js
+++ ble_captures/static/ble_captures/js/javascript.js
@@ -1,11 +1,4 @@
 
-/*
-function staticTest(){
-    alert("the static folder is set up properly");
-}
-
-document.getElementById("test").addEventListener("click",staticTest);
-*/
 
 document.addEventListener('DOMContentLoaded', function () {
 
@@ -28,6 +21,90 @@ document.addEventListener('DOMContentLoaded', function () {
 
 //Ongoing Attacks line chart
 //
+var chartDom = document.getElementById('ongoingAttacks');
+var myChart = echarts.init(chartDom);
+var option;
+
+function randomData() {
+  now = new Date(+now + oneDay);
+  value = value + Math.random() * 21 - 10;
+  return {
+    name: now.toString(),
+    value: [
+      [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'),
+      Math.round(value)
+    ]
+  };
+}
+let data = [];
+let now = new Date(1997, 9, 3);
+let oneDay = 24 * 3600 * 1000;
+let value = Math.random() * 1000;
+for (var i = 0; i < 1000; i++) {
+  data.push(randomData());
+}
+option = {
+  tooltip: {
+    trigger: 'axis',
+    formatter: function (params) {
+      params = params[0];
+      var date = new Date(params.name);
+      return (
+        date.getDate() +
+        '/' +
+        (date.getMonth() + 1) +
+        '/' +
+        date.getFullYear() +
+        ' : ' +
+        params.value[1]
+      );
+    },
+    axisPointer: {
+      animation: false
+    }
+  },
+  xAxis: {
+    type: 'time',
+    splitLine: {
+      show: false
+    }
+  },
+  yAxis: {
+    type: 'value',
+    boundaryGap: [0, '100%'],
+    splitLine: {
+      show: false
+    }
+  },
+  series: [
+    {
+      name: 'Fake Data',
+      type: 'line',
+      showSymbol: false,
+      data: data
+    }
+  ]
+};
+setInterval(function () {
+  for (var i = 0; i < 5; i++) {
+    data.shift();
+    data.push(randomData());
+  }
+  myChart.setOption({
+    series: [
+      {
+        data: data
+      }
+    ]
+  });
+}, 100);
+
+option && myChart.setOption(option);
+
+
+
+
+/*
 document.addEventListener("DOMContentLoaded", () => {
     new Chart(document.querySelector('#ongoingAttacks'), {
         type: 'line',
@@ -59,7 +136,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
-
+*/
 //Current Devices line chart
 document.addEventListener("DOMContentLoaded", () => {
     new Chart(document.querySelector('#deviceCount'), {
