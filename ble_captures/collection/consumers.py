cd8a7f193c39b271648ae1883dd191a860c73e18
Ryan <ryanmunger10@gmail.com>
2024-11-13 21:36:15 -0500

    Decode Heartbeat Messages

--- ble_captures/collection/consumers.py
+++ ble_captures/collection/consumers.py
@@ -38,10 +38,36 @@ class PacketDelivery:
     timestamp: int
     packets: List[BLEPacket]
 
+@dataclass
+class NetworkInfo:
+    interface_name: str
+    total_received: int
+    total_transmitted: int
+
+@dataclass
+class SystemInfo:
+    total_memory: float            # Total memory in GB
+    used_memory: float             # Used memory in GB
+    total_swap: float              # Total swap memory in GB
+    used_swap: float               # Used swap memory in GB
+    total_cpu_usage: float         # Total CPU usage as percentage
+    disk_info: List[str]           # List of disk info strings
+    network_info: List[NetworkInfo] # List of network information
+    packet_queue_length: int       # Length of packet queue
+
+@dataclass
+class HeartbeatMessage:
+    serial: int
+    timestamp: str
+    body: SystemInfo
+
 with open('./collection/packet_schema.avsc', 'r') as packet_schema_file:
     PACKET_SCHEMA = json.load(packet_schema_file)
 
-def decode_avro_packet(binary_data: bytes) -> PacketDelivery:
+with open('./collection/hb_schema.avsc', 'r') as hb_schema_file:
+    HB_SCHEMA = json.load(hb_schema_file)
+
+def decode_packet_delivery(binary_data: bytes) -> PacketDelivery:
     bytes_reader = io.BytesIO(binary_data)
     reader = fastavro.reader(bytes_reader, PACKET_SCHEMA)
     data = next(reader)
@@ -71,6 +97,38 @@ def decode_avro_packet(binary_data: bytes) -> PacketDelivery:
         packets=packets
     )
     
+def decode_heartbeat(binary_data: bytes) -> HeartbeatMessage:
+    bytes_reader = io.BytesIO(binary_data)
+    reader = fastavro.reader(bytes_reader, HB_SCHEMA)
+    data = next(reader)
+
+    # Map data to the NetworkInfo, SystemInfo, and HeartbeatMessage dataclasses
+    network_info = [
+        NetworkInfo(
+            interface_name=n['interface_name'],
+            total_received=n['total_received'],
+            total_transmitted=n['total_transmitted']
+        )
+        for n in data['body']['network_info']
+    ]
+
+    system_info = SystemInfo(
+        total_memory=data['body']['total_memory'],
+        used_memory=data['body']['used_memory'],
+        total_swap=data['body']['total_swap'],
+        used_swap=data['body']['used_swap'],
+        total_cpu_usage=data['body']['total_cpu_usage'],
+        disk_info=data['body']['disk_info'],
+        network_info=network_info,
+        packet_queue_length=data['body']['packet_queue_length']
+    )
+
+    return HeartbeatMessage(
+        serial=data['serial'],
+        timestamp=data['timestamp'],
+        body=system_info
+    )
+
 class SendPacketsSocket(WebsocketConsumer):
     def connect(self):
         self.accept()
@@ -82,9 +140,10 @@ class SendPacketsSocket(WebsocketConsumer):
         # print(bytes_data)
         print("Received Transmission")
         try:
-            packet_delivery = decode_avro_packet(bytes_data)
+            packet_delivery = decode_packet_delivery(bytes_data)
             print("First deserialized packet from delivery: ")
             print(packet_delivery.packets[0])
         except:
-            print("Heartbeat")
+            hb_msg = decode_heartbeat(bytes_data)
+            print(hb_msg)
 
